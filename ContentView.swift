eff3a260ba5a3f0b12eb3bd052c47b7443e4ae64
fjx <1060422838@qq.com>
2022-05-22 14:04:07 +0800

    添加了手势交互：旋转，移动和放缩

--- ContentView.swift
+++ ContentView.swift
@@ -12,19 +12,21 @@ import ARKit
 struct ContentView : View {
     
     @StateObject var arViewModel = ARViewModel()
-    @State var isPlacementEnable: Bool = false
     @State var selectedModel: String?
     @State var currentModel: String?
+    @State var placementEnabled: Bool = false
     var models: [String] = ["PegasusTrail","wateringcan"]
     
     var body: some View {
         ZStack(alignment: .bottom) {
+            
             ARViewContainer(currentModel: $currentModel)
-            if isPlacementEnable {
-                PlacementButtonView(isPlacementEnabled: $isPlacementEnable, selectedModel: $selectedModel, currentModel: $currentModel)
+            
+            if placementEnabled {
+                PlacementButtonView(placementEnabled: $placementEnabled, selectedModel: $selectedModel, currentModel: $currentModel)
             }
             else {
-                ModelPicker(isPlacementEnabled: $isPlacementEnable, selectedModel: $selectedModel, models: models)
+                ModelPicker(placementEnabled: $placementEnabled, selectedModel: $selectedModel, models: models)
             }
             
         }.environmentObject(arViewModel)
@@ -38,10 +40,11 @@ struct ARViewContainer: UIViewRepresentable {
     
     func makeUIView(context: Context) -> ARView {
         
-        let arView = ARView(frame: .zero)
+        let arView = arViewModel.arView
         let config = ARWorldTrackingConfiguration()
         config.planeDetection = [.horizontal,.vertical]
         config.environmentTexturing = .automatic
+        config.isLightEstimationEnabled = true
         if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
             config.sceneReconstruction = .mesh
         }
@@ -51,12 +54,16 @@ struct ARViewContainer: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {
+        
         if let modelName = currentModel {
             let fileName = modelName + ".usdz"
             let modelEntity = try!ModelEntity.loadModel(named: fileName)
+            modelEntity.generateCollisionShapes(recursive: true)
             let anchorEntity = AnchorEntity(plane: .any)
             anchorEntity.addChild(modelEntity)
+            arViewModel.arView.installGestures([.translation,.rotation,.scale],for:modelEntity)
             arViewModel.arView.scene.addAnchor(anchorEntity)
+//            每次更新UIView后将当前模型置为空
             DispatchQueue.main.async {
                 currentModel = nil
             }
@@ -68,21 +75,22 @@ struct ARViewContainer: UIViewRepresentable {
 
 struct ModelPicker : View{
     
-    @Binding var isPlacementEnabled: Bool
+    @Binding var placementEnabled: Bool
     @Binding var selectedModel: String?
     var models: [String]
     
     var body: some View {
+        
         ScrollView(.horizontal) {
             HStack(spacing: 30) {
+//                点击按钮时，将当前已选择的模型存在selectedModel中
                 ForEach(0..<self.models.count) {
                     index in
                     Button(action: {
-                        print("DEBUG: click model button\(self.models[index])")
-                        isPlacementEnabled = true
+                        placementEnabled = true
                         selectedModel = models[index]
                     }, label: {
-                        Image(uiImage: UIImage(named: models[index])!).resizable().frame(height:80).aspectRatio(1/1, contentMode: .fill).background(Color.white).cornerRadius(12)
+                        Image(uiImage: UIImage(named: models[index])!).resizable().frame(height:80).aspectRatio(1/1, contentMode: .fill).background(Color.white)
                     }).buttonStyle(PlainButtonStyle())
                 }
             }
@@ -93,27 +101,30 @@ struct ModelPicker : View{
 
 struct PlacementButtonView: View {
     
-    @Binding var isPlacementEnabled: Bool
+    @Binding var placementEnabled: Bool
     @Binding var selectedModel: String?
     @Binding var currentModel: String?
     
     var body: some View {
         HStack {
+            
+//            无论点击了哪个按钮，下一次的渲染中应该显示模型选择框，因此placementEnabled
             Button(action: {
-                print("DEBUG: click cancel")
-                isPlacementEnabled = false
+                placementEnabled = false
                 selectedModel = nil
             }, label: {
                 Image(systemName: "xmark").frame(width: 60, height: 60).font(.title).background(Color.white.opacity(0.75)).cornerRadius(30).padding(20)
             })
+            
+//            点击了对勾按钮后，将当前模型置为选择的模型
             Button(action: {
-                print("DEBUG: click confirm")
                 currentModel = selectedModel
-                isPlacementEnabled = false
+                placementEnabled = false
                 selectedModel = nil
             }, label: {
                 Image(systemName: "checkmark").frame(width: 60, height: 60).font(.title).background(Color.white.opacity(0.75)).cornerRadius(30).padding(20)
             })
+            
         }
     }
 }
